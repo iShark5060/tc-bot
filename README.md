ba8e20edb92be667d38919fcf086c0a3b1e792d9
Lutz Schwemer Panchez <shark@shark5060.net>
2025-02-09 06:33:39 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,83 @@
+# Discord Bot for Ark of War
+
+![Static Badge](https://img.shields.io/badge/Node.js-22.13.1-red?style=for-the-badge) ![Static Badge](https://img.shields.io/badge/Discord.js-14.0-blue?style=for-the-badge)
+
+Discord Bot based on the `Discord.js` framework using `Node.js` to enable `/commands` for the game [Ark of War](https://www.7piratesgames.com/ark.html)
+The bot itself is pretty basic and basically just follows along the [tutorial](https://discordjs.guide/) of the Discord.js framework.
+
+## About
+
+This is the 4th iteration of this bot by now. In the beginning it was "hacked together" (his words) by [Krylar](https://github.com/Krylar) and rewritten several times by me over the years.
+Outside of the game itself, the bot is pretty useless, and even inside the game it has since outlived most of its usefulness.
+
+The Bot lives in the Diplomacy of War Discord Server - one of the largest communities around the game (https://discord.gg/YMAhCNjkgp).
+
+## Requirements
+
+- Node.js
+- I recommend using PM2
+
+## Configuration
+
+You will need to create several files and fill them out in order to use the bot.
+
+config.json
+```
+{
+    "clientId": "123456", # Your userID.
+    "guildId": "123456", # Your ServerID
+	"channelId1": "123456", # Channel for Autoupdated Mopup info (Status)
+	"channelId2": "123456" # Channel for Autoupdated Mopup info (Timer)
+}
+```
+
+.env
+```
+TOKEN=YOURDISCORDBOTTOKENGOESHERE
+```
+
+Unless you have a copy of the Theorycrafters Google Docs sheet, you probably will not be able to use the Healing function.
+If you do - the API access credentials go into the client_secret.json
+
+client_secret.json
+```
+{
+  "type": "service_account",
+  "project_id": "your_project_id",
+  "private_key_id": "your_private_key_id",
+  "private_key": "-----BEGIN PRIVATE KEY-----so much of a private key here-----END PRIVATE KEY-----\n",
+  "client_email": "your@client.email",
+  "client_id": "123456",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "insertthegooglecerturlhere"
+}
+```
+
+## Running the App
+
+Installing nvm, node & npm run:
+```
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.13.1".
+nvm current # Should print "v22.13.1".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+```
+
+Installing and starting the Bot
+```
+# dowload all the dependencies listed in the package.json
+npm init
+
+# actually start the bot
+node .
+```
--- tc-bot.js
+++ tc-bot.js
@@ -6,6 +6,7 @@ const GoogleCredentials = require('./client_secret.json');
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
+const { channelId1, channelId2 } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
 client.GoogleSheet = new GoogleSpreadsheet('1ymnFE-wVxEqNV4CkoEHVowKcGHZYGouOUk_wCRBNzL4');
@@ -50,74 +51,76 @@ for (const file of eventFiles) {
 client.login(process.env.TOKEN);
 
 let mopupTimer = setInterval(function() {
-    // so we basically do the /mopup command every 1 min. could be cleaner, but I can't be bothered.
-    // Channel names can only be edited twice every 10 mins, so we're just doing it once.
-    // output should be "<muIcon> Mopup is <muActive>, for <muTime>."
+	if (channelId1 && channelId2) {
+		// so we basically do the /mopup command every 1 min. could be cleaner, but I can't be bothered.
+		// Channel names can only be edited twice every 10 mins, so we're just doing it once.
+		// output should be "<muIcon> Mopup is <muActive>, for <muTime>."
 
-    let muIcon;
-    let muActive;
-    let muTime;
+		let muIcon;
+		let muActive;
+		let muTime;
 
-    let starttime;
-    let endtime;
-    const ttoday = Date.now();
-    const timeoffset = new Date().getTimezoneOffset();
-    const thours = Math.ceil((ttoday + timeoffset * 60 * 1000) / (60 * 60 * 1000)) - 8;
-    const today = Math.floor(thours / 24);
+		let starttime;
+		let endtime;
+		const ttoday = Date.now();
+		const timeoffset = new Date().getTimezoneOffset();
+		const thours = Math.ceil((ttoday + timeoffset * 60 * 1000) / (60 * 60 * 1000)) - 8;
+		const today = Math.floor(thours / 24);
 
-    if (today % 2 == 0) {
-        // multiplier: minutes * minutes * days ; offset is 26 hours after day start of the day from server reset
-        starttime = (today * 60 * 60 * 24 + 24 * 60 * 60) * 1000;
-        endtime = starttime + 8 * 60 * 60 * 1000;
-    }
-    else {
-        // multiplier: minutes * minutes * days ; offset is 8 hours after day start of the day from server reset
-        starttime = (today * 60 * 60 * 24 + 8 * 60 * 60) * 1000;
-        endtime = starttime + 16 * 60 * 60 * 1000;
-    }
+		if (today % 2 == 0) {
+			// multiplier: minutes * minutes * days ; offset is 26 hours after day start of the day from server reset
+			starttime = (today * 60 * 60 * 24 + 24 * 60 * 60) * 1000;
+			endtime = starttime + 8 * 60 * 60 * 1000;
+		}
+		else {
+			// multiplier: minutes * minutes * days ; offset is 8 hours after day start of the day from server reset
+			starttime = (today * 60 * 60 * 24 + 8 * 60 * 60) * 1000;
+			endtime = starttime + 16 * 60 * 60 * 1000;
+		}
 
-    // calculate time difference between now (utctime) and starttime
-    // convert utctime into unix
-    const currenttime = (new Date(new Date().toISOString()).valueOf() / 1000).toFixed(0) * 1000;
+		// calculate time difference between now (utctime) and starttime
+		// convert utctime into unix
+		const currenttime = (new Date(new Date().toISOString()).valueOf() / 1000).toFixed(0) * 1000;
 
-    const deltastart = starttime - currenttime;
-    const deltaend = endtime - currenttime;
+		const deltastart = starttime - currenttime;
+		const deltaend = endtime - currenttime;
 
-    if (deltastart < 0) {
-        if (deltaend > 0) {
-            muActive = 'ACTIVE';
-            muIcon = '🟢';
+		if (deltastart < 0) {
+			if (deltaend > 0) {
+				muActive = 'ACTIVE';
+				muIcon = '🟢';
 
-            // calculate remaining time window, use deltaend and convert into hh:mm:ss
-            muTime = new Date(deltaend).toISOString().slice(11, 19);
-        }
-        else {
-            muActive = 'INACTIVE';
-            muIcon = '🔴';
+				// calculate remaining time window, use deltaend and convert into hh:mm:ss
+				muTime = new Date(deltaend).toISOString().slice(11, 19);
+			}
+			else {
+				muActive = 'INACTIVE';
+				muIcon = '🔴';
 
-            // calculate next window start, depends of current day
-            if (today % 2 == 0) {
-                // cannot happen,
-            }
-            else {
-                muTime = new Date(deltaend + 24 * 60 * 60).toISOString().slice(11, 19);
+				// calculate next window start, depends of current day
+				if (today % 2 == 0) {
+					// cannot happen,
+				}
+				else {
+					muTime = new Date(deltaend + 24 * 60 * 60).toISOString().slice(11, 19);
 
-            }
-        }
-    }
-    else {
-        muActive = 'INACTIVE';
-        muIcon = '🔴';
+				}
+			}
+		}
+		else {
+			muActive = 'INACTIVE';
+			muIcon = '🔴';
 
-        // calculate the time remaining, use deltastart and convert into hh:mm:ss
-        muTime = new Date(deltastart).toISOString().slice(11, 19);
-    }
+			// calculate the time remaining, use deltastart and convert into hh:mm:ss
+			muTime = new Date(deltastart).toISOString().slice(11, 19);
+		}
 
-    // edit the channel:
-    const channel = client.channels.cache.get('1223400455577538660');
-    const channel2 = client.channels.cache.get('1223408725398655067');
-    channel.setName(`${muIcon} Mopup is: ${muActive}`)
-        .catch(console.error);
-    channel2.setName(`Time remaining: ${muTime}`)
-        .catch(console.error);
+		// edit the channel:
+		const channel = client.channels.cache.get(channelId1);
+		const channel2 = client.channels.cache.get(channelId2);
+		channel.setName(`${muIcon} Mopup is: ${muActive}`)
+			.catch(console.error);
+		channel2.setName(`Time remaining: ${muTime}`)
+			.catch(console.error);
+	}
 }, 1000*60*5);
