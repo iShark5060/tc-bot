aa97a12a52fe891173de6af91c7e1c4625acba0a
Lutz Schwemer Panchez <shark@shark5060.net>
2025-08-23 20:29:17 +0200

    Ping now has latency

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,10 +3,11 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong as well at latency.'),
 	examples: ['/ping'],
 
 	async execute(interaction) {
 		await interaction.reply('Pong!');
+		await interaction.editReply(`Pong! Latency is ${Date.now() - interaction.createdTimestamp}ms. API latency is ${Math.round(interaction.client.ws.ping)}ms`);
 	},
 };
