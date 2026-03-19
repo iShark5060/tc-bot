92986ee63cd6bcc8ed2019750d207a2c77995da8
Lutz Schwemer Panchez <shark@shark5060.net>
2026-03-20 00:40:16 +0100

    chore: update ecosystem configuration and package dependencies, reduce max restarts, and add native module build step in deployment workflow

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       merge_logs: true,
       autorestart: true,
-      max_restarts: 10,
+      max_restarts: 3,
       min_uptime: '10s',
       listen_timeout: 10000,
       kill_timeout: 5000,
